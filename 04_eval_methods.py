a03c3d06482092f9ad887e7c95ec4bcd1489c393
Christoph Heim <Christoph.Heim@meteoswiss.ch>
2018-05-24 14:29:37 +0200

    Create evaluation script. wip.

--- 04_eval_methods.py
+++ 04_eval_methods.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 import pickle
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
 
 
 ############ USER INPUT #############
@@ -12,10 +14,12 @@ obs_case_name = 'burglind'
 model_case_name = 'burglind_ref'
 # obs model combination case name
 obs_model_case_name = 'OBS_'+obs_case_name+'_MODEL_'+model_case_name
-# mode of plotting: either ALL_STATIONS (1 plot for each station) or MEAN_OVER_STATIONS
-#plot_mode = 'ALL_STATIONS'
-#plot_mode = 'MEAN_OVER_STATIONS'
-plot_mode = 'ABO'
+# mode of evaluation: either ALL_STATIONS (1 plot for each station) or MEAN_OVER_STATIONS
+#eval_mode = 'ALL_STATIONS'
+#eval_mode = 'MEAN_OVER_STATIONS'
+eval_mode = 'ABO'
+# should a plot be created?
+i_create_plot = 0
 # save output (1) or plot output (0)
 i_save = 0
 # gust methods to calculate and plot
@@ -26,25 +30,20 @@ i_save = 0
 i_methods = [1,3,4]
 # path of input obs_model pickle file
 data_pickle_path = '../data/'+obs_model_case_name+'.pkl'
-# directory to save plots
 plot_base_dir = '../plots/'
 plot_case_dir = plot_base_dir + obs_model_case_name + '/'
 MODEL = 'model'
 OBS = 'obs'
-unit = 'km/h'
-#unit = 'm/s'
 #####################################
 
 # create directories
 if i_save and not os.path.exists(plot_case_dir):
     os.mkdir(plot_case_dir)
 
-quit()
-
 # load data
 data = pickle.load( open(data_pickle_path, 'rb') )
 
-station_names = data['station_names']
+station_names = np.asarray(data['station_names'])
 nstat = len(station_names)
 nmethods = len(i_methods)
 
@@ -55,41 +54,22 @@ for i in range(0,nhrs):
     hr_inds[i,:] = i*360 + np.arange(0,360)
 hr_inds = hr_inds.astype(np.int)
 
-#print(data[MODEL]['ABO']['tcm'].shape[0]/360)
-#quit()
 
 # value fields
 mod_gust = np.full((nhrs,nstat,4),np.nan)
 obs_gust = np.full((nhrs,nstat),np.nan)
-mod_wind = np.full((nhrs,nstat),np.nan)
-obs_wind = np.full((nhrs,nstat),np.nan)
-# hypothetical gust that model method 1 calculates from observed wind
-hypot_gust = np.full((nhrs,nstat),np.nan)
-# error fields
-mod_err_gust = np.full((nhrs,nstat,4),np.nan)
-abs_err_gust = np.full((nhrs,nstat,4),np.nan)
-# k bra
-k_bra = np.full((nhrs,nstat),np.nan)
+#mod_wind = np.full((nhrs,nstat),np.nan)
+#obs_wind = np.full((nhrs,nstat),np.nan)
 
 
 # loop through all stations
 for si,stat in enumerate(station_names):
 
-    # unit conversion
-    if unit == 'km/h':
-        data[MODEL][stat]['zvp10'] = data[MODEL][stat]['zvp10']*3.6
-        data[MODEL][stat]['zvp30'] = data[MODEL][stat]['zvp30']*3.6
-        data[MODEL][stat]['zvpb'] = data[MODEL][stat]['zvpb']*3.6
-        data[MODEL][stat]['zv_bra'] = data[MODEL][stat]['zv_bra']*3.6
-        data[OBS][stat]['VMAX_10M1'] = data[OBS][stat]['VMAX_10M1']*3.6
-        data[OBS][stat]['FF_10M'] = data[OBS][stat]['FF_10M']*3.6
-
     for hr in range(0,nhrs):
         inds = hr_inds[hr]
         obs_gust[hr,si] = data[OBS][stat]['VMAX_10M1'][hr] 
-        mod_wind[hr,si] = np.mean(data[MODEL][stat]['zvp10'][inds])
-        obs_wind[hr,si] = data[OBS][stat]['FF_10M'][hr] 
-        k_bra[hr,si] = np.mean(data[MODEL][stat]['k_bra'][inds])
+        #mod_wind[hr,si] = np.mean(data[MODEL][stat]['zvp10'][inds])
+        #obs_wind[hr,si] = data[OBS][stat]['FF_10M'][hr] 
 
     for mi in i_methods:
 
@@ -102,15 +82,6 @@ for si,stat in enumerate(station_names):
             zsqcm = np.sqrt(zcm)
             zvp10 = data[MODEL][stat]['zvp10']
             gust = zvp10 + 3.0 * 2.4 * zsqcm * zvp10
-
-            # hypothetical observation based gust
-            hyp_gust_fact = 3.0 * 2.4 * zsqcm 
-            for hr in range(0,nhrs):
-                inds = hr_inds[hr]
-                hr_max_hyp_gust_fact = np.max(hyp_gust_fact[inds])
-                hyp_gust = obs_wind[hr,si] + hr_max_hyp_gust_fact * obs_wind[hr,si]
-                hypot_gust[hr,si] = hyp_gust
-
 
         if mi == 2:
 
@@ -133,11 +104,9 @@ for si,stat in enumerate(station_names):
             zcm[zcm < 5E-4] = 5E-4
             zsqcm = np.sqrt(zcm)
             zvp10 = data[MODEL][stat]['zvp10']
-            if unit == 'km/h':
-                gust = zvp10 + (3.0 * 2.4 + 0.09 * zvp10/3.6) * zsqcm * zvp10
-            elif unit == 'm/s':
-                gust = zvp10 + (3.0 * 2.4 + 0.09 * zvp10) * zsqcm * zvp10
+            gust = zvp10 + (3.0 * 2.4 + 0.09 * zvp10) * zsqcm * zvp10
     
+        # calc and save model max gust
         for hr in range(0,nhrs):
             inds = hr_inds[hr]
             # find maximum gust
@@ -146,185 +115,114 @@ for si,stat in enumerate(station_names):
             mod_gust[hr,si,mi-1] = hr_max_gust
 
 
-
+# calculate scores
+# error fields
+mod_err_gust = np.full((nhrs,nstat,4),np.nan)
+abs_err_gust = np.full((nhrs,nstat,4),np.nan)
 for mi in i_methods:
     mod_err_gust[:,:,mi-1] = mod_gust[:,:,mi-1] - obs_gust
 abs_err_gust = np.abs(mod_err_gust)
 
+#stat = 'SAE'
+title = '500 stations'
+si = np.argwhere(station_names == stat).squeeze()
+si = np.arange(0,500)
+#si = 1
+#print(si)
+
+###################### PLOT ABSOLUTE ERROR
+# regression
+reg = LinearRegression(fit_intercept=True)
+
+# plot preparation
+fig,axes = plt.subplots(1,3,figsize=(14,4))
+ymax = -np.Inf
+ymin = np.Inf
+
+# loop over axes and gust calc method
+for i,mi in enumerate(i_methods):
+    ax = axes[i]
+
+    # prepare feature matrix
+    X = obs_gust[:,si].flatten().reshape(-1,1)
+    y = mod_err_gust[:,si,mi-1].flatten()
+    #print(X.shape)
+    #print(y.shape)
+
+    # remove nans
+    mask = np.isnan(X[:,0])
+    #mask[np.isnan(y)] = True
+    X = X[~mask]
+    y = y[~mask]
+
+    # determine max/min y
+    ymax = max(np.max(y),ymax)
+    ymin = min(np.min(y),ymin)
+
+    # fit regression and draw line
+    reg.fit(X,y)
+    line = reg.predict(X)
+
+    # plotting
+    ax.scatter(obs_gust[:,si], mod_err_gust[:,si,mi-1], color='black', marker=".")
+    ax.plot(X, line, color='red')
+    ax.set_xlabel('Observed gust (OBS) [m/s]')
+    if i == 0:
+        ax.set_ylabel('Model absolute error (MOD-OBS) [m/s]')
+    ax.axhline(0, color='k', linewidth=0.8)
+    ax.set_title('Method '+str(mi))
+# set axes limits in each ax
+for ax in axes:
+    ax.set_ylim((ymin,ymax))
+
+# finish plot
+plt.suptitle(title)
+plt.show()
 
 
-############################################################################
-################################# PLOTTING #################################
-############################################################################
+###################### CALCULATE ERROR MEASURES
+# REGRESSION ERROR MEASURES
+error_measures = {}
+error_measures['mean_abs_err'] = np.full((nstat,nmethods), np.nan)
+error_measures['r2'] = np.full((nstat,nmethods), np.nan)
+error_measures['explained_var'] = np.full((nstat,nmethods), np.nan)
+#sis = np.arange(0,600)
+for si in range(0,nstat):
+    y_obs = obs_gust[:,si]
 
-if plot_mode == 'ALL_STATIONS':
-    for si,stat in enumerate(station_names):
-        print('station ' + stat)
-        lines = []
-        labels = []
+    # find nans
+    mask = np.isnan(y_obs)
+    y_obs = y_obs[~mask]
 
-        fig = plt.figure(figsize=(12,8))
-        # model gusts
-        for i in range(0,4):
-            if i+1 in i_methods:
-                line, = plt.plot(mod_gust[:,si,i])
-                lines.append(line)
-                labels.append('gust method '+str(i+1))
-        # observed gust
-        line, = plt.plot(obs_gust[:,si],color='black')
-        lines.append(line)
-        labels.append('gust obs')
-        # model mean wind
-        line, = plt.plot(mod_wind[:,si])
-        lines.append(line)
-        labels.append('wind model')
-        # observed mean wind
-        line, = plt.plot(obs_wind[:,si])
-        lines.append(line)
-        labels.append('wind obs')
-        # hypothetical model gust method 1
-        # based on observed wind
-        line, = plt.plot(hypot_gust[:,si], linestyle='--')
-        lines.append(line)
-        labels.append('hypoth. gust')
+    for i,mi in enumerate(i_methods):
+        y_mod = mod_gust[:,si,mi-1].flatten()
+        y_mod = y_mod[~mask]
+        error_measures['mean_abs_err'][si,i] = metrics.mean_absolute_error(y_obs, y_mod)
+        error_measures['r2'][si,i] = metrics.r2_score(y_obs, y_mod)
+        error_measures['explained_var'][si,i] = metrics.explained_variance_score(y_obs, y_mod)
 
-        ax = plt.gca()
-
-        ax.set_xlabel('simulation hour')
-        ax.set_ylabel('wind/gust ['+unit+']')
-        ax.set_title(stat)
-        ax.grid()
-
-        ax2 = ax.twinx()
-        line, = ax2.plot(k_bra[:,si], linestyle='-', linewidth=1.0, color='grey')
-        lines.append(line)
-        labels.append('k bra')
-
-        ax2.set_ylabel('brasseur model level')
-        ax2.set_ylim((20,80))
-
-        plt.legend(lines,labels)
-
-        plot_name = plot_case_dir + stat + '.png'
-
-        if i_save == 0:
-            plt.show()
-        else:
-            plt.savefig(plot_name)
-            plt.close('all')
+# CATEGORICAL ERROR MEASURES
 
 
-
-
-
-
-elif plot_mode == 'MEAN_OVER_STATIONS':
-    mod_gust_stat_mean = np.nanmean(mod_gust, axis=1)
-    obs_gust_stat_mean = np.nanmean(obs_gust, axis=1)
-    mod_wind_stat_mean = np.nanmean(mod_wind, axis=1)
-    obs_wind_stat_mean = np.nanmean(obs_wind, axis=1)
-    hypot_gust_stat_mean = np.nanmean(hypot_gust, axis=1)
-
-    lines = []
-    labels = []
-    # model gusts
-    for i in range(0,4):
-        if i+1 in i_methods:
-            line, = plt.plot(mod_gust_stat_mean[:,i])
-            lines.append(line)
-            labels.append('gust method '+str(i+1))
-    # observed gust
-    line, = plt.plot(obs_gust_stat_mean,color='black')
-    lines.append(line)
-    labels.append('gust obs')
-    # model mean wind
-    line, = plt.plot(mod_wind_stat_mean)
-    lines.append(line)
-    labels.append('wind model')
-    # observed mean wind
-    line, = plt.plot(obs_wind_stat_mean)
-    lines.append(line)
-    labels.append('wind obs')
-    # hypothetical model gust method 1
-    # based on observed wind
-    line, = plt.plot(hypot_gust_stat_mean, linestyle='--')
-    lines.append(line)
-    labels.append('hypoth. gust')
-
-    plt.legend(lines,labels)
-    
-    ax = plt.gca()
-
-    ax.set_xlabel('simulation hour')
-    ax.set_ylabel('wind/gust ['+unit+']')
-    ax.set_title(stat)
-
-    plot_name = plot_case_dir + 'all' + '.png'
-
-    if i_save == 0:
-        plt.show()
+###################### PLOT REGRESSION ERROR MEASURES
+fig,axes = plt.subplots(1,3,figsize=(14,4))
+i = 0
+for key,meas in error_measures.items():
+    ax = axes[i]
+    if key == 'mean_abs_err':
+        bins = np.arange(0,20.1,1)
     else:
-        plt.savefig(plot_name)
-        plt.close('all')
-
-
-
-# plot a single station
-else:
-    stat = plot_mode
-    if stat in station_names:
-        print('station ' + stat)
-        lines = []
-        labels = []
-
-        fig = plt.figure(figsize=(12,8))
-        # model gusts
-        for i in range(0,4):
-            if i+1 in i_methods:
-                line, = plt.plot(mod_gust[:,si,i])
-                lines.append(line)
-                labels.append('gust method '+str(i+1))
-        # observed gust
-        line, = plt.plot(obs_gust[:,si],color='black')
-        lines.append(line)
-        labels.append('gust obs')
-        # model mean wind
-        line, = plt.plot(mod_wind[:,si])
-        lines.append(line)
-        labels.append('wind model')
-        # observed mean wind
-        line, = plt.plot(obs_wind[:,si])
-        lines.append(line)
-        labels.append('wind obs')
-        # hypothetical model gust method 1
-        # based on observed wind
-        line, = plt.plot(hypot_gust[:,si], linestyle='--')
-        lines.append(line)
-        labels.append('hypoth. gust')
-
-        ax = plt.gca()
-
-        ax.set_xlabel('simulation hour')
-        ax.set_ylabel('wind/gust ['+unit+']')
-        ax.set_title(stat)
-        ax.grid()
-
-        ax2 = ax.twinx()
-        line, = ax2.plot(k_bra[:,si], linestyle='-', linewidth=1.0, color='grey')
-        lines.append(line)
-        labels.append('k bra')
-
-        ax2.set_ylabel('brasseur model level')
-        ax2.set_ylim((20,80))
-
-        plt.legend(lines,labels)
-
-        plot_name = plot_case_dir + stat + '.png'
-
-        if i_save == 0:
-            plt.show()
-        else:
-            plt.savefig(plot_name)
-            plt.close('all')
+        bins = np.arange(-1,1.1,0.1)
+    ax.hist(meas,bins=bins, histtype='step',
+            color=['red','orange','black'], label=['Method 1', 'Method 2', 'Method 4'])
+    ax.axvline(0,color='k')
+    if key == 'mean_abs_err':
+        ax.legend(loc='upper right')
+        ax.set_xlabel('Absolute error [m/s]')
     else:
-        raise ValueError('Unknown station name or option given by user for variable "plot_mode"!') 
+        ax.legend(loc='upper left')
+        ax.set_xlabel(']-inf,1]')
+    ax.set_title(key)
+    i += 1
+plt.show()
+
